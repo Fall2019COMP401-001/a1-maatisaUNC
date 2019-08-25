ca8cdf0277c4cb818a57b3b398943885fd5b1058
maatisaUNC <54414378+maatisaUNC@users.noreply.github.com>
2019-08-25 19:10:12 -0400

    Got rid of the java system line

--- src/a1/A1Example.java
+++ src/a1/A1Example.java
@@ -9,17 +9,15 @@ public class A1Example {
 		// you know what Eclipse thinks it is using and
 		// so that you can confirm that it is 10.0 or higher.
 		
-		System.out.println("Your Java version: " + System.getProperty("java.version"));
-		
+
 		// Set up scanner object to read space-separated values
 		// from console input
-		
+
 		Scanner scan = new Scanner(System.in);
 
 		// Read in count of numbers to process
-		
+
 		int count = scan.nextInt();
-		
 		
 		// Create an array to store numbers.
 		
